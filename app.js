1390ce82f5b05fbe34b1c2cbd5d312f97dbf75b9
mlcas <martincastro88@hotmail.com>
2021-09-21 17:25:56 -0700

    subiendo el proyecto con la inclusion de formularios

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const publicPath = path.join(__dirname,"./public");// aca ya tengo la ruta publi
 app.use(express.static(publicPath))
 
 
-app.listen(3030, () => {
+app.listen(3000, () => {
     console.log("Servidor Corriendo en el puerto 3030")
 });
 
@@ -16,3 +16,23 @@ app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname, "./views/home.html"));
  
  })
+
+ app.get("/register",(req,res)=>{
+    res.sendFile(path.join(__dirname, "./views/register.html"));
+ 
+ })
+
+ app.get("/login",(req,res)=>{
+    res.sendFile(path.join(__dirname, "./views/login.html"));
+ 
+ })
+
+ app.post("/register",(req,res)=>{
+    res.redirect("/"); // aca tenes que poner la misma ruta que la fila 15 que es app.get("/")
+ 
+ })
+ app.post("/login",(req,res)=>{
+    res.redirect("/"); // aca tenes que poner la misma ruta que la fila 15 que es app.get("/")
+ 
+ })
+
